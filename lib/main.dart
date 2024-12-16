74855f020639425094c49ed01a4cfec7c61d720e
ghost08A <126407230+ghost08A@users.noreply.github.com>
2024-12-16 11:35:13 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true, appBarTheme: AppBarTheme(color: Colors.blue)),
       //colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(221, 236, 15, 15)),
       home: Scaffold(
-        appBar: AppBar(
+        /*appBar: AppBar(
           centerTitle: true,
           title: Image.asset(
             'assets/images/peaLOGO.png',
@@ -26,97 +26,299 @@ class MyApp extends StatelessWidget {
           // actions: [IconButton(onPressed: (){debugPrint('clicked menu buttton');}, icon:const Icon(Icons.safety_check)),
           //           IconButton(onPressed: (){debugPrint('clicked menu buttton');}, icon:const Icon(Icons.offline_bolt)),
           //           ],
+        ),*/
+
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Name",
+                hintText: ' Enter Name',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.home), iconSize: 40),
+                    Text("Home",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.search),
+                        iconSize: 40),
+                    Text("search",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.info), iconSize: 40),
+                    Text("Information",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                  ],
+                )
+              ],
+            ),
+            /*content*/
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.start, // Align texts to the start
+                      children: [
+                        Text(
+                          "วันที่ชาร์จ", // First Text widget
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30),
+                        ),
+                        SizedBox(
+                            height: 10), // Adds spacing between the two texts
+                        Text(
+                          "วันที่ชาร์จ", // Second Text widget
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.start, // Align texts to the start
+                      children: [
+                        Text(
+                          "วันที่ชาร์จ", // First Text widget
+                          style: TextStyle(
+                               fontSize: 20),
+                        ),
+                        SizedBox(
+                            height: 10), // Adds spacing between the two texts
+                        Text(
+                          "วันที่ชาร์จ", // Second Text widget
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Text(
+                      "วันที่ชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Text(
+                      "วันที่ชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Text(
+                      "วันที่ชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Text(
+                      "วันที่ชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Text(
+                      "วันที่ชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Text(
+                      "วันที่ชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]),
         ),
-        body: Column(
-          children: [
-            Image.asset(
-              'assets/images/PEA1.jpg',
-            ),
-            const Text("ขอบคุณที้ใช้บริการ",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            const Text("เรายินดีที่ได้เป็นส่วนหนึ่งในการเดินทางของคุณ",
-                style: TextStyle(
-                  fontSize: 20,
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Text("สรุปรายละเอียด",
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                //   ElevatedButton(onPressed: (){debugPrint('clicked buttton');}, child: const Text('OK',style: TextStyle(fontSize: 20),)),
-                // IconButton(onPressed:(){},
-                // icon: Icon(Icons.favorite),iconSize: 40,)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                //ElevatedButton(onPressed: (){debugPrint('clicked buttton');}, child: const Text('OK',style: TextStyle(fontSize: 20),)),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.calendar_month),
-                  iconSize: 40,
-                ),
-                Text(
-                  "วันที่ชาร์จ",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                Spacer(),
-                Text(
-                  "9/12/2567",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                IconButton(
+
+        /*body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+             mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/images/PEA1.jpg',
+              ),
+              const Text("ขอบคุณที้ใช้บริการ",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              const Text("เรายินดีที่ได้เป็นส่วนหนึ่งในการเดินทางของคุณ",
+                  style: TextStyle(
+                    fontSize: 20,
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("สรุปรายละเอียด",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  //   ElevatedButton(onPressed: (){debugPrint('clicked buttton');}, child: const Text('OK',style: TextStyle(fontSize: 20),)),
+                  // IconButton(onPressed:(){},
+                  // icon: Icon(Icons.favorite),iconSize: 40,)
+                ],
+              ),
+             
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  //ElevatedButton(onPressed: (){debugPrint('clicked buttton');}, child: const Text('OK',style: TextStyle(fontSize: 20),)),
+                  
+                  IconButton(
                     onPressed: () {},
-                    icon: Icon(Icons.ev_station),
-                    iconSize: 40),
-                Text("สถานีชาร์จ",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                Spacer(),
-                Text("PEA VOLIA บางจาก",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-              ],
+                    icon: Icon(Icons.calendar_month),
+                    iconSize: 40,
+                  ),
+                  Expanded(child: 
+                  Card(child:  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+                    child: Text(
+                    "วันที่ชาร์จ",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),),),),
+                   
+                  
+                  Spacer(),
+                  Text(
+                    "9/12/2567",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.ev_station),
+                      iconSize: 40),
+                  Text("สถานีชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                  Spacer(),
+                  Text("PEA VOLIA บางจาก",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.bolt), iconSize: 40),
+                  Text("ประเภทหัวชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                  Spacer(),
+                  Text("CCS2",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.access_time),
+                      iconSize: 40),
+                  Text("ระยะเวลาการชาร์จ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                  Spacer(),
+                  Text("00:12:32",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                ],
+              ),
+              const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Name",
+                hintText: ' Enter Name',
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                IconButton(
-                    onPressed: () {}, icon: Icon(Icons.bolt), iconSize: 40),
-                Text("ประเภทหัวชาร์จ",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                Spacer(),
-                Text("CCS2",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.access_time),
-                    iconSize: 40),
-                Text("ระยะเวลาการชาร์จ",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                Spacer(),
-                Text("00:12:32",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-              ],
-            ),
-          ],
-        ),
+            ],
+            
+          ),
+        ), */
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             debugPrint('FAB pressed');
@@ -128,52 +330,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'arm count down:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ));
-  }
-}
 
 // void main(){
 //   runApp(const MaterialApp(home:Scaffold(body: Text('\n\n\n\nHello world')) ));
